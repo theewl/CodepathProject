6a680b4d7ccc13c49d5b82268005c3e8773a813d
Eric Liang <liange3@uci.edu>
2019-05-23 17:30:32 -0700

    .

---
+++ example_project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  example_project
+//
+//  Created by Eric Liang on 5/23/19.
+//  Copyright © 2019 theEWL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
