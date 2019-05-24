e384d3539c6e2e2f21d6e06e3c308e3dfdb3daa1
Eric Liang <liange3@uci.edu>
2019-05-23 17:40:46 -0700

    added eric file

---
+++ example_project/EricTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EricTableViewCell.swift
+//  example_project
+//
+//  Created by Eric Liang on 5/23/19.
+//  Copyright © 2019 theEWL. All rights reserved.
+//
+
+import UIKit
+
+class EricTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
